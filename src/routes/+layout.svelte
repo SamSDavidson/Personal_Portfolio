f3f38c75fb4867c58337dc34ee7580f307cdc374
sam <sam.davidson@snhu.edu>
2024-02-28 10:26:53 -0500

    button style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,25 +12,25 @@
 		<AppBar>
 			<div class="relative inline-block text-left ">
 				<div
-					class="z-10 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none "
+					class="z-10 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
 					role="menu"
 					aria-orientation="vertical"
 					aria-labelledby="menu-button"
 					tabindex="-1"
 				>
 					<div class="py-1" role="none">
-						<a class="btn btn-sm border-r-2 border-dotted border-black" href="/" rel="noreferrer">
+						<a class="btn btn-sm" href="/" rel="noreferrer">
 							Home
 						</a>
 						<a
-							class="btn btn-sm border-r-2 border-dotted border-black"
+							class="btn btn-sm"
 							href="https://github.com/SamSDavidson"
 							target="_blank"
 							rel="noreferrer"
 							>GitHub
 						</a>
 						<a
-							class="btn btn-sm border-r-2 border-dotted border-black"
+							class="btn btn-sm "
 							href="mailto:samdavidson@skiff.com"
 							target="_blank"
 							rel="noreferrer"
@@ -38,16 +38,16 @@
 							Email Me
 						</a>
 						<a
-							class="btn btn-sm border-r-2 border-dotted border-black"
+							class="btn btn-sm"
 							href="/education"
 							rel="noreferrer"
 						>
 							Education
 						</a>
-						<a class="btn btn-sm border-r-2 border-dotted border-black" href="/personal" rel="noreferrer">
+						<a class="btn btn-sm" href="/personal" rel="noreferrer">
 							Personal Projects
 						</a>
-						<a class="btn btn-sm border-r-2 border-dotted border-black" href="/playbook" rel="noreferrer">
+						<a class="btn btn-sm" href="/playbook" rel="noreferrer">
 							Cybersecurity Playbook
 						</a>
 						<div class="btn btn-sm">
